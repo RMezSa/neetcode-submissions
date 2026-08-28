43691e26878f814fdaa0ffc8d03e209fe6243b2a
Roberd Meza <rbdego@gmail.com>
2026-08-28 16:01:07 -0600

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> respuesta(n);
+
+        int zeros = 0;
+        int index_cero = 0;
+        long long producto = 1;
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0){
+                zeros++;
+                index_cero = i;    
+            }else{
+                producto *= nums[i];
+            }
+
+        }
+
+        if(zeros > 1){
+            return respuesta;
+        }else if (zeros == 1){
+            respuesta[index_cero] = producto;
+        }else{
+            for(int i = 0; i < n; i++){
+                respuesta[i] = producto*(pow(nums[i], -1));
+            }
+        }
+        return respuesta;
+    }
+};
