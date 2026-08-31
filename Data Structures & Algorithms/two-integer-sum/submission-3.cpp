a5e72c5b09e13e4c822b4f5c39fdb562fdbf5c5f
Roberd Meza <rbdego@gmail.com>
2026-08-31 09:31:36 -0600

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int, int> dict;
+
+        vector<int> resultado(2);
+
+        for(int i = 0; i < n; i++){
+            if(dict.find(target - nums[i]) != dict.end()){
+                resultado[0] = dict[target - nums[i]];
+                resultado[1] = i;
+                return resultado;
+            }
+            dict[nums[i]] = i;
+        }
+
+        return resultado;
+    }
+};
